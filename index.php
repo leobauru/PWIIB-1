bd7765ed6e3980c387596b002499457a781f6132
Lab05_etec <Lab05_etec@etec>
2025-02-21 22:40:07 -0300

    navBar_02

--- index.php
+++ index.php
@@ -7,137 +7,69 @@
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="bootstrap.min.css">
-    <style>
+    <link rel="stylesheet" href="bootstrap-icons.min.css">
+    <!-- <style>
       .row{
         border:2px solid red;
         
       }
 
 
-    </style>
+    </style> -->
 
 
 </head>
 <body>
 
     
-<!-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="produtos.php">Produtos</a>
-        </li>
-        <li class="nav-item ">
-          <a class="nav-link dropdown-toggle" href="contato.php" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Contatos
-          </a>
-        </li>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+<!-- <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+<div class="container">
+    <a class="navbar-brand" href="#">
+    <i class="bi bi-apple"></i>
+    <img src="/docs/5.3/assets/apple.svg" alt="Bootstrap" width="30" height="24">
+    </a>
   </div>
-</nav> -->
-
-  <!-- <h1>sistema de layout</h1>
-  <div class="row justify-content-center text-center">
-    <div class="col-4 border ">red</div>
-  </div>
-
-  <div class="col">
-    <div class="row-5 border"></div>
-  </div> -->
-
-<div class="row text-center">
   <button type="button" class="btn col-3 end btn-outline-primary">Cadastras</button>
-  <div class="col-3 ">Pesquisar</div>
+  <div class="col-3 text-light center">Pesquisar</div>
   <div type="button" class="btn col-3 btn-outline-primary">
     <input type="text" class="form-control" id="floatingInputGroup1" placeholder="Username"/>
     </div>
   <div class="col-1">
   </div>
   <button type="button" class="btn col-2 btn-outline-primary">Procurar</button>
-</div>
+</nav> -->
 
 
+<nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div class="container-fluid">
+    
+    <a class="navbar-brand" href="#">
+      <img src="/docs/5.3/assets/iconn.npg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+      Bootstrap
+    </a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
+        
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            php
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="variavel.php">variavel</a></li>
+            <li><a class="dropdown-item" href="if.php">iF</a></li>
+            <li><a class="dropdown-item" href="while.php">while</a></li>
+            <li><a class="dropdown-item" href="for.php">for</a></li>
+            <li><a class="dropdown-item" href="switch.php">switch</a></li>
+            <li><a class="dropdown-item" href="array.php">array/vetor</a></li>
+          </ul>
 
-        <!-- <div class="alert alert-success" role="alert">
-            A simple success alert—check it out!
-        </div> -->
-
-    <!-- Button trigger modal
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button> -->
-
-<!-- Modal -->
-<!-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </ul>
   </div>
-</div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+</nav>
 
-<br>
-<div class="row justify-content-center text-center">
-  <div class="col-10">
 
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td colspan="2">Larry the Bird</td>
-        <td>@twitter</td>
-      </tr>
-    </tbody>
-  </table>    <script src=".\bootstrap.bundle.min.js "></script>
-  </div>
-</div> -->
+
+
+    <script src=".\bootstrap.bundle.min.js "></script>
+
 </html>
 </body>
