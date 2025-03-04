e1117ae86ac8c656940cc2c332c00665e16f7c34
Matheus Luna <matheuslunacamargo@gmail.com>
2025-03-04 17:15:06 -0300

    exercicio If

--- if.php
+++ if.php
@@ -1,158 +1,187 @@
-<?php
-    include "./nav.php";
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>If</title>
-    <link href="bootstrap.min.css" rel="stylesheet">
+    <title>Projeto PWII B</title>
+    <link href="bootstrap.min.css" rel="stylesheet" />
+    <link href="estilo.css" rel="stylesheet" />
 </head>
-<body style="overflow-x:hidden">
-    <H1>IF</H1>
-    <h2>Estrutura de decisão</h2>
-    <p>
-        A estrutura de decisão verifica se é verdadeiro faça uma coisa, 
-        caso seja falso faça soutra
-    </p>
-    <pre>
-        if($numero == 1){
-            echo" verdadeiro: $numero é igual a 1";
-        }else{
-            echo " falso: $numero é diferente de 1";
-        }
-    </pre>
-    <p>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+
+                <img src="/pwiib/imagens/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24" />
+                Projeto PW 2 Turma B
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            php
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="variavel.php">Variável</a></li>
+                            <li><a class="dropdown-item" href="if.php">If</a></li>
+                            <li><a class="dropdown-item" href="while.php">While</a></li>
+                            <li><a class="dropdown-item" href="for.php">For</a></li>
+                            <li><a class="dropdown-item" href="switch.php">Switch</a></li>
+                            <li><a class="dropdown-item" href="array.php">Array/Vetor</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <h1>if</h1>
+        <p>
+        <h2>Estrutura de decisão</h2>
+        A estrutura de decisão é composta por um teste booleano.
+        Esse teste verifica uma condição se verdadeiro faça alguma
+        ou se falso faça outra.
+        <pre>
+if($numero == 1)
+{
+  echo "Aqui significa $numero igual a 1";
+}else{
+  echo "Aqui significa $numero diferente de 1";
+}
+</pre>
 
         <?php
-            $numero = rand(0,1);
-            if($numero == 1){
-                echo" verdadeiro: $numero é igual a 1";
-            }else{
-                echo " falso: $numero é diferente de 1";
-            }
+        $numero = 2;
+        if ($numero == 1) {
+            echo "Aqui significa $numero igual a 1";
+        } else {
+            echo "Aqui significa $numero diferente de 1";
+        }
+        ?>
+        </p>
+
+        <h2> Estrutura de decisão encadeada</h2>
+        <p>
+            São varios testes um seguido do outro até algum teste verdadeiro ser encontrado
+            Caso não encontre nenhum positivo o else é executado.
+            Ao encontrar um teste positivo todos os subsequentes não serão executados.
+        <pre>
+$nome = "abc";
+if($nome == "João"){
+
+}else if($nome == "maria"){
+
+}else if($nome == "Fernando"){
+    echo "Achou o nome Fernando e encerra o IF";
+}else if($nome == "Pedro"){
+
+}else{
+  echo "nenhum nome é igual ao conteudo da variavel";
+}
+</pre>
+
+
+        <?php
+        $nome = "abc";
+        if ($nome == "João") {
+        } else if ($nome == "maria") {
+        } else if ($nome == "Fernando") {
+            echo "Achou o nome Fernando e encerra o IF";
+        } else if ($nome == "Pedro") {
+        } else {
+            echo "nenhum nome é igual ao conteudo da variavel";
+        }
+        ?>
+
+        <h2>Vários testes em um 1 if </h2>
+        Em uma estrutura de decisão unica eu consigo realizar dois ou mais testes.
+        Nesse caso eu preciso utilizar os conectivos AND ou OR ( E ou OU ).
+        <h3>Conectivo E (And)</h3>
+        No exemplo abaixo eu necessito que toda as condições resultem em verdadeiro
+        <pre>
+  $numero2 = 35;
+  if($numero2 > 10 && $numero2 < 50)
+  {
+      echo "$numero2 está entre 10 e 50";
+  }
+
+  //       FALSE            TRUE
+  if($numero2 > 36 && $numero2 < 50)
+  {
+      //Esse código não será executado
+  }else{
+    echo "$numero2 não está entre 36 e 50";
+  }
+</pre>
+
+        <?php
+        $numero2 = 35;
+        //        true   e      true
+        if ($numero2 > 10 && $numero2 < 50) {
+            echo "$numero2 está entre 10 e 50";
+        }
+
+        //       FALSE            TRUE
+        if ($numero2 > 36 && $numero2 < 50 && $numero2 == 35) {
+            //Esse código não será executado
+        } else {
+            echo "$numero2 não está entre 36 e 50";
+        }
+
         ?>
     </p>
-
-
-
-<pre>
-    $nome = "fernando";
-
-    if($nome == "maria"){
-        
-    }else if($nome == "fernando"){
-        
-    }else if($nome =="pedro"){
-        
-    }else{
-        echo " nenhum nome é igual ao conteudo da variavel";
+        <h3>Conectivo OU (or) </h3>
+        <p>
+            O conectivo ou testa todas as condições e executa o código se apenas uma
+            das condições for verdadeira. Caso nenhum condição for verdadeiro o else é
+            executado.
+                <br />
+            <pre>
+    $telefone = "14998885214";
+    if($telefone == "14998885214" || $telefone == "1688885555")
+    {
+        echo "Aqui um dos telefone é igual a variavel";
     }
-</pre>
+            </pre>
+            Quando temos várias condições apenas uma é necessario ao usar o conectivo OU
+            representado por dois PIPE (Tecla que fica do lado do Z) ||
+        </p>
 
-<p>
+        <h2>Exercicio</h2>
+        <p>
+            Crie uma variavel idade preencha com um valor e
+            escreva na tela se ela pode ou não entrar em um evento
+            considerando as seguintes regras:
+            <ul>
+                <li>Menor de 18 anos: Não pode entrar.</li>
+                <li>Maior ou igual a 18 e menor que 60: Pode entrar.</li>
+                <li>Maior ou igual a 60: Entrada gratuita.</li>
+            <ul>
 
-    <?php
-        $nome = "df";
-        // $nomes = {
-            //     entidade01: "matheus";
-            // }
-            if($nome == "maria"){
-                echo "nao";
-            }else if($nome == "fernando"){
-                echo "nao";
-                
-            }else if($nome =="pedro"){
-                echo "nao";
-                
-            }else{
-                echo " nenhum nome é igual ao conteudo da variavel <br>";
-            }
-    ?>
-</p>
+            <?php
+                $idade = rand(5, 75);
+                if ($idade < 18) {
+                    echo "<span>Não pode entrar.</span>";
+                } else if ($idade >= 18 && $idade < 60) {
+                    echo "<span>Pode entrar.</span";
+                } else {
+                    echo "<span>Entrada gratuita.</span>";
+                }
+            ?>
+        </p>
+    </div>
 
-
-
-
-<h2>
-    varios testes em um If
-</h2>
-
-<pre>
-    $numero2 = 35;
-    if(($numero2 > 10 && $numero2 < 50) || $numero2 == number ){
-        echo " $numero2 esta entre 10 e 50.";
-    }
-
-    if($numero2 > 36 && $numero2 < 50){
-        // nao sera executado
-    }else{
-        echo "$numero2 não está entre 36 e 50";
-    }
-</pre>
-
-<?php 
-
-$numero2 = 35;
-if($numero2 > 10 && $numero2 < 50  ){
-    echo " $numero2 esta entre 10 e 50. <br>";
-}
-
-if($numero2 > 36 && $numero2 < 50){
-    // nao sera executado
-}else{
-    echo "$numero2 não está entre 36 e 50";
-}
-
-?>
-
-
-<h3>Or</h3>
-
-<p>
-    o conectivo "ou" testa todas as condições e executa o código se apenas uma das condições for verdadeira. 
-    Caso nenhuma condição for verdadeiro o else é executado.
-</p>
-<br>
-<pre>
-    $tell = "14998729070";
-    if($tell =* "14998729070" || $tell == "14991575958"){
-        echo "Aqui um dos telefone [e igual a variavel";
-    }
-</pre>
-<p>
-
-    Quando temos várias condições apenas uma é necessario ao usar o conectivo ao " OU " representado por dois PIPE 
-    (Tecla que fica do lado do z)
-</p>
-
-<h2>Exercicio</h2>
-<p>
-    Crie uma variavel idade preencha com um valor e
-    escreva na tela se ela pode ou não entrar em um evento
-    considerando as seguintes regras:
-<ul>
-    <li>Menor de 18 anos: Não pode entrar.</li>
-    <li>Maior ou igual a 18 e menor que 60: Pode entrar.</li>
-    <li>Maior ou igual a 60: Entrada gratuita.</li>
-<ul>
-
-<?php
-    $idade = rand(5,75);
-    if($idade < 18){
-        echo "<span>Não pode entrar.</span>";
-    }else if($idade >= 18 && $idade < 60){
-        echo "<span>Pode entrar.";
-    }else{
-        echo "Entrada gratuita.";
-    }
-?>
-
-</p>
-
-
-    <script src="bootstrap.bundle.min.js" ></script>
+    <script src="bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
